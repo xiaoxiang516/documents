5e4d0fd4f9c592afbe76d00fabf6d8eb2bd5a7d7
xiaoxiang <7959773+xiaoxiang516@user.noreply.gitee.com>
2022-10-10 22:20:11 +0800

    update

---
+++ 黑客文化/pagesExtension/myscript.js
@@ -0,0 +1,27 @@
+console.log("chrome00://extensions/");
+console.log(chrome,899988);
+
+function delPageAdverte() {
+	
+}
+// 确保每个函数功能非常简单，一眼能看明白，是代码可维护的关键。廖雪峰
+(function () {	
+	$("#asdjiwww").remove();
+	$("#HMRichBox").remove();
+	$(".rightToolBar").remove();
+	$(".yddiv.clDiv").remove();
+	$("#lb-win").remove();
+	$("#HMRichBox").remove();
+	// $("#").remove();
+	$("#HMcoupletDivleft").remove();
+	$("#HMcoupletDivright").remove();
+	$("#HMRichBox").remove();
+	$(".adsbygoogle.adsbygoogle-noablate").remove();
+	$("#aswift_7_anchor").remove();
+	$("#sponsor-a").remove();
+	// http://www.qihuys803.com/
+	$(".footfix").remove();
+	$("#a2").remove();
+	//  获取兄弟节点指定标签
+	$(".container > a").remove()
+})();
